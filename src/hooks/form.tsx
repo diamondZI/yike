ecf51b9a7475f4ae68a14ed0b69cf3614be67b17
Your Name <2680456922@qq.com>
2023-09-04 21:20:42 +0800

    change

--- src/hooks/form.tsx
+++ src/hooks/form.tsx
@@ -1,21 +1,3 @@
-// import second from 'first'
-// const { data, loading, loadingMore, loadMore } = useInfiniteScroll(service);
-const data=['1', '2', '3', '4', '5', '6', '7', '8', '9']
-import {useEffect, useState} from 'react'
-// const { value, setValue, backLength, forwardLength, back, forward } = useHistoryTravel<string>()
-import {useRef} from 'react'
-function useInfiniteScroll(service:Function) {
-  const [value,setValue]  =useState('')
-  const baclAction =[]
-  const forwardAction =[]
-  useEffect(()=>{
-
-    
-  },[value])
-  const back=(e:any)=>{
-
-    setValue(value)
-  }
-  const forward=[]
-  // return { data, loading, loadingMore, loadMore }
+export default function a(){
+  return "!"
 }
