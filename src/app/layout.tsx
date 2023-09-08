66064a0504ae71d9fb13c7a0fcbc0a44fd4d1040
Your Name <2680456922@qq.com>
2023-09-08 15:45:37 +0800

    delete mate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,7 @@ import Nav from '@/components/nav'
 import Footer from '@/components/footer'
 import {usePathname} from 'next/navigation'
 import {useAutoAnimate} from '@formkit/auto-animate/react'
-export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
-}
+
 export default function RootLayout({
   children,
 }: {
